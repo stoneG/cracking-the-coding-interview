53213f8edac7fdceb6436b6d5d1d8221eb159ee3
stoneG <me@sitong>
2013-01-25 17:10:21 -0800

    adding non space constrained case

--- 1-1.py
+++ 1-1.py
@@ -1,6 +1,17 @@
+# Time: O(n)
+# Space: O(n)
 def all_unique(string):
+    character = {}
+    for char in string:
+        if character.get(char):
+            return False
+    return True
+
+# Time: O(n^2)
+# Space: O(1)
+def all_unique2(string):
     for i, char in enumerate(string):
-        for j in range(len(string)):
-            if i != j and char == string[j]:
+        for other_char in string[i+1:]:
+            if char == other_char:
                 return False
     return True
