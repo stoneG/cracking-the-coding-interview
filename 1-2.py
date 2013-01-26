9e27ea6a99b5261c2733bbefbf0f491c51f1f8de
stoneG <me@sitong>
2013-01-25 19:37:37 -0800

    extended answer

--- 1-2.py
+++ 1-2.py
@@ -1,4 +1,10 @@
 # Time: O(n)
-# Space: O(n)
+# Space: O(1)
 def reverse(string):
     return ''.join([string[i] for i in range(len(string)-1, -1, -1)])
+
+# More Pythonic, and includes the null-termination
+# Time: O(n)
+# Space: O(1)
+def reverse(string):
+    return string[-2::-1] + '\0'
