8c019003898332e32472489297008f75e00fe5ba
stoneG <me@sitong>
2013-01-28 15:51:44 -0800

    binary search tree vs binary tree

---
+++ 4-5.py
@@ -0,0 +1,37 @@
+from binary_tree import Node
+
+def is_bst(root, min=float('-inf'), max=float('inf')):
+    left, right, value = root.left, root.right, root.value
+    if min <= value <= max:
+        if left and right:
+            return is_bst(left, min, value) and is_bst(right, value, max)
+        elif left:
+            return is_bst(left, min, value)
+        elif right:
+            return is_bst(right, value, max)
+        return True
+    return False
+
+"""
+BST
+      4
+   2     5
+ 1   3 5   6
+
+BT
+      4
+   2     5
+ 1   8 5   6
+"""
+
+if __name__ == '__main__':
+    bst = Node(4)
+    bst.left, bst.right = Node(2), Node(5)
+    bst.left.left, bst.left.right = Node(1), Node(3)
+    bst.right.left, bst.right.right = Node(5), Node(6)
+    bt = Node(4)
+    bt.left, bt.right = Node(2), Node(5)
+    bt.left.left, bt.left.right = Node(1), Node(8)
+    bt.right.left, bt.right.right = Node(5), Node(6)
+    print 'is_bst(bst) =', is_bst(bst)
+    print 'is_bst(bt) =', is_bst(bt)
