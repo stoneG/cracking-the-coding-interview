3b2e5500e29b7fe11914db1866ed33de6b4e76cf
stoneG <me@sitong>
2013-01-28 16:04:23 -0800

    removing duplicate nodes

--- 4-5.py
+++ 4-5.py
@@ -2,7 +2,7 @@ from binary_tree import Node
 
 def is_bst(root, min=float('-inf'), max=float('inf')):
     left, right, value = root.left, root.right, root.value
-    if min <= value <= max:
+    if min < value < max:
         if left and right:
             return is_bst(left, min, value) and is_bst(right, value, max)
         elif left:
@@ -15,23 +15,23 @@ def is_bst(root, min=float('-inf'), max=float('inf')):
 """
 BST
       4
-   2     5
- 1   3 5   6
+   2     6
+ 1   3 5   7
 
 BT
       4
-   2     5
- 1   8 5   6
+   2     6
+ 1   8 5   7
 """
 
 if __name__ == '__main__':
     bst = Node(4)
-    bst.left, bst.right = Node(2), Node(5)
+    bst.left, bst.right = Node(2), Node(6)
     bst.left.left, bst.left.right = Node(1), Node(3)
-    bst.right.left, bst.right.right = Node(5), Node(6)
+    bst.right.left, bst.right.right = Node(5), Node(7)
     bt = Node(4)
-    bt.left, bt.right = Node(2), Node(5)
+    bt.left, bt.right = Node(2), Node(6)
     bt.left.left, bt.left.right = Node(1), Node(8)
-    bt.right.left, bt.right.right = Node(5), Node(6)
+    bt.right.left, bt.right.right = Node(5), Node(7)
     print 'is_bst(bst) =', is_bst(bst)
     print 'is_bst(bt) =', is_bst(bt)
